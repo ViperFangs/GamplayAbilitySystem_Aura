dce3ce622f319029ae308ddff1c291aac66bb370
ViperFangs <amathrej@asu.edu>
2026-03-08 04:33:07 -0700

    Character Blueprint Setup
    - Add Weapon Property to AuraCharacterBase
    - Initialize a Default Skeletal Mesh Component Subobject for Weapon
    - Attach the weapon to a socket on the mesh
    - Disable Weapon Collision
    - Add BP_AuraCharacter.uasset
    - Add BP_Goblin_Spear.uasset

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -6,7 +6,14 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
+	
+	// Initialize a Subobject for a Weapon
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	// Attach the weapon to a socket on the Skeleton Mesh
+	// GetMesh is a function inherited from the ACharacter class 
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	// Disable weapon collision
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
