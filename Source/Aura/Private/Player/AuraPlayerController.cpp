d8cca48c452464c520512ad59173e6e98e8f7c53
ViperFangs <amathrej@asu.edu>
2026-03-08 20:25:09 -0700

    Aura Player Controller
    - Add EnhancedInput to the PublicDependencyModuleNames in Aura.Build.cs
    - Add AuraPlayerController.cpp
        - Include EnhancedInputSubsystems Header Files
        - Set Replicate to true
        - Grab the Local Player Subsystem by using UEnhancedInputLocalPlayerSubsystem
        - Bind the Aura Input Mapping Context to the Local Player
        - Set an Input Mode that recieves input from both the game and the UI
        - Show the default mouse cursor to the player

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,40 @@
 
 #include "Player/AuraPlayerController.h"
 
+#include  "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	// Allows a change to be replicated to all clients.
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	// If input is not working, error out.
+	// Assert that the AuraContext pointer is resolved.
+	check(AuraContext);
+	
+	// Get the Enhanced Input subsystem that belongs to "this" player.
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	// Assert that the Subsystem pointer is resolved.
+	check(Subsystem);
+
+	// Add the AuraContext to "this player".
+	// AuraContext contains the default input for the player.
+	Subsystem->AddMappingContext(AuraContext, 0);
+	
+	// Show the mouse cursor
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	// Create an Input Mode to receive player input from both the game and the UI.
+	FInputModeGameAndUI InputModeData;
+	// Do not lock the mouse to the viewport.
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	// Do not hide the cursor when it is in the viewport.
+	InputModeData.SetHideCursorDuringCapture(false);
+	// Apply the mouse settings to the player controller
+	SetInputMode(InputModeData);
+}
