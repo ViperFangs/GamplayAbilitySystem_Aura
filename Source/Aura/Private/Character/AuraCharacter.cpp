97f763f642c6cf8174316de000cd21b5a94a19e8
ViperFangs <amathrej@asu.edu>
2026-03-10 00:15:11 -0700

    Implement AuraCharacter Constructor
    - Set Default Third Person View Values for CharacterMovement
    - Decouple character rotation from camera rotation

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,21 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// Character should face in the direction it moves
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// Rotates the Yaw at a set speed 
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+	// Lock movement to a flat plane
+	GetCharacterMovement()->bConstrainToPlane = true;
+	// Snap to the plane immediately on start
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	
+	// Let bOrientRotationToMovement handle rotation instead of the controller handling the rotation
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
